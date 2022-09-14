659421b3f63fac57dfcc46641d55c85c4ac0a382
Rafael Marini <rfaelmarini@gmail.com>
2022-09-14 15:41:05 -0300

    feat: add resource list route

--- src/modules/Resource/ResourceRepository.js
+++ src/modules/Resource/ResourceRepository.js
@@ -3,3 +3,10 @@ const Resource = require('./Resource');
 exports.create = async (resourceInfo) => {
     await Resource.create(resourceInfo);
 };
+
+exports.find = async (filter, pageSize, page, projection) => {
+    return await Resource.find(filter, projection)
+        .sort({ active: -1 })
+        .skip(page > 0 ? ((page - 1) * pageSize) : 0)
+        .limit(pageSize)
+}
