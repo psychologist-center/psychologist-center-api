ab7649a62248b139a29cec24653344c81803fb36
Rafael Marini <rfaelmarini@gmail.com>
2022-09-14 16:28:49 -0300

    feat: add resource edit route

--- src/modules/Resource/ResourceController.js
+++ src/modules/Resource/ResourceController.js
@@ -57,3 +57,40 @@ exports.listResources = async (req, res) => {
         res.status(400).json({ message: e.message });
     }
 };
+
+exports.editResource = async (req, res) => {
+    let { id } = req.params;
+    let { title, description, category, current_user } = req.body;
+
+    let contract = new ValidationContract();
+    contract.isRequired(title, "O campo título não pode ser vazio");
+    contract.isRequired(category, "O campo categoria não pode ser vazio");
+    contract.isValidCategory(
+        category,
+        "O campo categoria deve possuir um dos seguintes valores: metáfora, transe ou ferramenta"
+    );
+
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
+    const where = {
+        _id: id,
+        user_id: current_user,
+    };
+
+    try {
+        await ResourceRepository.update(where, {
+            title,
+            description,
+            category,
+        });
+
+        res.status(200).json({ message: "Dados atualizados com sucesso" });
+
+        patient = [];
+    } catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+};
